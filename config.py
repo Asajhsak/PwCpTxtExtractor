5dc690e0e4b591a69673e3c5945b5580bbce6a45
Asajhsak <shoonyadigital12@gmail.com>
2025-05-07 14:26:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "23237831"
-api_hash = "017dc661ca1432ac2fe5ecb62499d88f"
-bot_token = ""
+api_id = "29787754"
+api_hash = "b0823caff3fe7927c203850656ca1b51"
+bot_token = "7714596806:AAFoUOuv_OzwgFp6H4HTLoxewN2XnbspnSk"
 auth_users = [6103594386]
